c71c1b4d4afeb8bb2fb56a4a2562656a5f1a80de
Sankalp Sangle <sankalpsangle@lawn-143-215-58-183.lawn.gatech.edu>
2021-10-16 17:12:19 -0400

    Deleted zips, added clean files

--- p2/clean.py
+++ p2/clean.py
@@ -1,6 +1,6 @@
 for i in range(2,9):
-    file = open("omp-example-" + str(i) + "t-log.out","r")
-    newfile = open("cleaned-omp-example-"+str(i)+"t-log.out", "x")
+    file = open("results/openmpresults/sense_reversal_outputs/omp-example-" + str(i) + "t-log.out","r")
+    newfile = open("results/openmpresults/sense_reversal_outputs/cleaned-omp-example-"+str(i)+"t-log.out", "x")
     newfile.write("Barrier,ThreadID,Time\n")
     Lines = file.readlines()
     for line in Lines:
