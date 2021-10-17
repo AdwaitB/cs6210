e08058ec96107e9b576bd04ce2f33fab0c42f7c7
Sankalp S Sangle <ssangle6@login-coc-ice-1.pace.gatech.edu>
2021-10-17 02:06:21 -0400

    Added a master script

---
+++ p2/tests/combinedtests/combined/masterscript.sh
@@ -0,0 +1,20 @@
+cd 2threads
+rm combined_*
+./runJobsCombined.sh
+cd ../4threads
+rm combined_*
+./runJobsCombined.sh
+cd ../6threads
+rm combined_*
+./runJobsCombined.sh
+cd ../8threads
+rm combined_*
+./runJobsCombined.sh
+cd ../10threads
+rm combined_*
+./runJobsCombined.sh
+cd ../12threads
+rm combined_*
+./runJobsCombined.sh
+cd ..
+
